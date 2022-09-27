ac584396e3c262cbc3c54c433c6c6d698b68a4d9
Vadim B <vadim.bobrovitsky@gmail.com>
2022-09-27 13:16:55 +0300

    Cleanup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Kion</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='We deliver more than just the features to manage and govern your cloud' />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
