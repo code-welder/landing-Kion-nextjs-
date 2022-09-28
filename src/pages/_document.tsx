3407106f180b5a17cfb1da161663ba03db529bd5
Vadim B <vadim.bobrovitsky@gmail.com>
2022-09-28 20:12:46 +0300

    Add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
