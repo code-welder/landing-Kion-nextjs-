3799127fe32c589cbd114e372bbf7bf9a0243876
Vadim B <vadim.bobrovitsky@gmail.com>
2022-09-30 12:16:43 +0300

    Add generate react cli

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kion landing page with NextJS
+
+Create React components with cli: **[more info](https://www.npmjs.com/package/generate-react-cli)**
+
+```bash
+npx generate-react-cli component ComponentName
+```
---
+++ templates/component/TemplateName.tsx
@@ -0,0 +1,13 @@
+import styles from './TemplateName.module.scss';
+
+interface TemplateNameProps {}
+
+function TemaplateName(props: TemplateNameProps) {
+  return (
+    <div className={styles.wrapper}>
+      
+    </div>
+  );
+};
+
+export default TemaplateName;
