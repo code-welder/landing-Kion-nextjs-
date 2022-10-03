e6ca7705d891166b8697213d3b2080904e2e20aa
Vadim B <vadim.bobrovitsky@gmail.com>
2022-10-03 12:25:15 +0300

    Update next & ts configs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  // https://react-svgr.com/docs/webpack/#use-svgr-and-asset-svg-in-the-same-project
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      type: 'asset',
+      resourceQuery: /url/, // *.svg?url
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
