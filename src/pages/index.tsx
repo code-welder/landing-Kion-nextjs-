0ebd441ce8255e6645a5d56f0efa24b74e8ee378
Vadim B <vadim.bobrovitsky@gmail.com>
2022-10-03 16:05:38 +0300

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
+import Header from '@components/Header';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Kion</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta name='description' content='We deliver more than just the features to manage and govern your cloud' />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <meta
+          name="description"
+          content="We deliver more than just the features to manage and govern your cloud"
+        />
       </Head>
+      <Header />
     </>
   );
 };
