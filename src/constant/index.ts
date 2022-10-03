f696081b582e30a4441c0637dec7ca574c2185f2
Vadim B <vadim.bobrovitsky@gmail.com>
2022-10-03 14:36:03 +0300

    Update constants & scss mixins

---
+++ src/constant/index.ts
@@ -0,0 +1,10 @@
+// Equal media mixins from `./src/styles/_mixins.scss`
+
+const mediaQueries = {
+  mobile: '(max-width: 600px)',
+  tablet: '(max-width: 1024px)',
+
+  onlyTablet: '(min-width: 601px) and (max-width: 1024px)',
+};
+
+export { mediaQueries };
