2b23854e17c3d2fd4ac44755185f47876d2f47c2
Vadim B <vadim.bobrovitsky@gmail.com>
2022-10-03 18:04:00 +0300

    Add Hero section

---
+++ src/components/SectionHero/SectionHero.tsx
@@ -0,0 +1,68 @@
+import clsx from 'clsx';
+import Image from 'next/image';
+import styles from './SectionHero.module.scss';
+import Button from '@components/Button';
+
+import MainImage from '@images/hero-section/main.png';
+import StatImage1 from '@images/hero-section/stat-1.png';
+import StatImage2 from '@images/hero-section/stat-2.png';
+import StatImage3 from '@images/hero-section/stat-3.png';
+
+function SectionHero() {
+  return (
+    <section className={styles.wrapper}>
+      <div className={clsx(styles.inner, 'container')}>
+        <div className={styles.text}>
+          <h1 className="section-title">
+            Complete cloud enablement has arrived.
+          </h1>
+          <p className={styles.description}>
+            Oversee, secure, and scale your cloud with one solution.
+            <Button
+              className={styles.demo}
+              size="lg"
+            >
+              Request Demo
+            </Button>
+          </p>
+        </div>
+
+        <div className={styles.media}>
+          <div className={styles.media__mainImg}>
+            <Image
+              src={MainImage}
+              alt="#"
+              priority
+              layout="responsive"
+            />
+          </div>
+          <div className={clsx(styles.media__stat, styles.media__stat1)}>
+            <Image
+              src={StatImage1}
+              alt="#"
+              layout="responsive"
+            />
+          </div>
+
+          <div className={clsx(styles.media__stat, styles.media__stat2)}>
+            <Image
+              src={StatImage2}
+              alt="#"
+              layout="responsive"
+            />
+          </div>
+
+          <div className={clsx(styles.media__stat, styles.media__stat3)}>
+            <Image
+              src={StatImage3}
+              alt="#"
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SectionHero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
 import Header from '@components/Header';
+import SectionHero from '@components/SectionHero';
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +18,10 @@ const Home: NextPage = () => {
         />
       </Head>
       <Header />
+
+      <main>
+        <SectionHero />
+      </main>
     </>
   );
 };
