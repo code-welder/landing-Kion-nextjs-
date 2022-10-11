c970d892e66008cb5011b2b864cdfdfb45554d01
Vadim B <vadim.bobrovitsky@gmail.com>
2022-10-12 00:05:14 +0300

    Add NewsSubscribeForm component

---
+++ src/components/NewsSubscribeForm/NewsSubscribeForm.tsx
@@ -0,0 +1,41 @@
+import Button from '@components/Button';
+import clsx from 'clsx';
+import { FormEvent } from 'react';
+import styles from './NewsSubscribeForm.module.scss';
+
+interface NewsSubscribeFormProps {
+  className?: string;
+}
+
+function NewsSubscribeForm({ className }: NewsSubscribeFormProps) {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      className={clsx(styles.wrapper, className)}
+      onSubmit={handleSubmit}
+    >
+      <h2 className={styles.title}>Sign up for our monthly newsletter</h2>
+
+      <fieldset className={styles.inner}>
+        <input
+          type="email"
+          name="email"
+          className={styles.input}
+          placeholder="Email*"
+          required
+        />
+        <Button
+          className={styles.button}
+          type="submit"
+        >
+          Subscribe
+        </Button>
+      </fieldset>
+    </form>
+  );
+}
+
+export default NewsSubscribeForm;
